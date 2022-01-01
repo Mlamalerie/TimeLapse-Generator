65d28314120086441473c507e30a1ea3f8c09087
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2022-01-01 20:59:27 +0100

    Version 1, notif, sh exe

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import shutil
 import time
 import pyautogui
 import cv2
-import win32api
+from plyer import notification
 
-from param import SECRET_SCREENSHOTS_DIR_PATH,VIDEO_FPS,RESULTS_DIR_PATH
+from param import SECRET_SCREENSHOTS_DIR_PATH, VIDEO_FPS, RESULTS_DIR_PATH
+
 
 def CreerDossierSauvegarde(where, doss=None):
     nomEmplacementSauvegarde = where + "/" + doss if doss else where
@@ -16,27 +17,34 @@ def CreerDossierSauvegarde(where, doss=None):
     else:
         return nomEmplacementSauvegarde
 
+
 # **** EMPLACEMENT SAUVEGARDE
 SECRET_SCREENSHOTS_DIR_PATH = CreerDossierSauvegarde(SECRET_SCREENSHOTS_DIR_PATH)
 # **** NOM SAUVEGARDE
 CACHE_DIR = CreerDossierSauvegarde(SECRET_SCREENSHOTS_DIR_PATH, "cache_timelapse")
 username = os.getlogin()
 
-def run_screen(delay_sec,limit_time_hour = 24,compt_sec = 0):
-    convert_sec_to_hour = lambda s : s/3600
-    if convert_sec_to_hour(compt_sec) < limit_time_hour :
-        #print("convert_sec_to_hour(compt_sec) ", convert_sec_to_hour(compt_sec))
+def convert_sec_to_hour(s):
+    return s / 3600
+
+def run_screen(delay_sec, limit_time_hour=24, compt_sec=0):
+
+    if convert_sec_to_hour(compt_sec) < limit_time_hour:
+        # print("convert_sec_to_hour(compt_sec) ", convert_sec_to_hour(compt_sec))
         screen()
         time.sleep(delay_sec)
-        run_screen(delay_sec,limit_time_hour,compt_sec + 1)
+        run_screen(delay_sec, limit_time_hour, compt_sec + 1)
     else:
-        print(f"... run_screen() stop, time is over ! ({convert_sec_to_hour(compt_sec)}h")
+        print(f"... run_screen() stop, time is over !")
+        return convert_sec_to_hour(compt_sec)
+
 
 def screen():
     nomFichier = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S') + " " + username
     photo = CACHE_DIR + "/" + nomFichier + ".png"
     pyautogui.screenshot(photo)
     print("~ " + photo)
+
 
 def remove(path):
     # removing the folder
@@ -48,59 +56,62 @@ def remove(path):
             # failure message
             print(f" * Unable to delete the {path}")
 
+def notify(title="...", message="...."):
+    image = os.getcwd() + "/img/time-lapse.ico"
+    print(image)
+    notification.notify(title=title, message=message, timeout=10, app_icon=image)
 
-def create_timelapse(image_folder_path,params =  ""):
+def create_timelapse(image_folder_path, params="",nb_test_rest = 3):
     images = sorted([img for img in os.listdir(image_folder_path) if img.endswith(".png")])
 
-    if len(images) > 3:
-        video_name = f'timelapse - {images[0].split(" ")[0]} - {images[-1].split(" ")[0]} [{params}].mp4'
-        video_name = f"{RESULTS_DIR_PATH}/{video_name}"
+    if nb_test_rest > 0:
+        if len(images) > 3:
+            video_name = f'timelapse - {images[0].split(" ")[0]} - {images[-1].split(" ")[0]} [{params}].mp4'
+            video_name = f"{RESULTS_DIR_PATH}/{video_name}"
 
-        try:
-            frame = cv2.imread(os.path.join(image_folder_path, images[0]))
-            height, width, layers = frame.shape
-            size = (width, height)
-            video = cv2.VideoWriter(video_name, 0x7634706d, VIDEO_FPS, size)
+            try:
+                frame = cv2.imread(os.path.join(image_folder_path, images[0]))
+                height, width, layers = frame.shape
+                size = (width, height)
+                video = cv2.VideoWriter(video_name, 0x7634706d, VIDEO_FPS, size)
 
-            for image in images:
-                video.write(cv2.imread(os.path.join(image_folder_path, image)))
+                for image in images:
+                    video.write(cv2.imread(os.path.join(image_folder_path, image)))
 
-            cv2.destroyAllWindows()
-            video.release()
-            print("~ video saved at",video_name)
-        except:
-            print("error in video creation...",e)
+                cv2.destroyAllWindows()
+                video.release()
+            except:
+                print("error in video creation...")
+                print("let's try again ...")
+                create_timelapse(image_folder_path, params, nb_test_rest-1)
+            else:
+                print("~ video saved at", video_name)
+                notify("Timelapse video saved at", f"-> {video_name}")
+                remove(CACHE_DIR)
+        else:
+            print("not enough images...")
     else:
-        print("not enough images...")
-
-def on_exit(signal_type):
-    print('caught signal:', str(signal_type))
-    remove(CACHE_DIR)
+        print("not enough nb try...")
 
 def main():
-    win32api.SetConsoleCtrlHandler(on_exit, True)
-
     TIME_MAX_HOUR = int(input(" * TIME MAX (HOUR) > "))
-    SCREENSHOT_DELAY_SEC = int(input(" * SCREENSHOT DELAY (SEC) [5, 15, 30] > "))
-    print(" * go screens")
+    #TIME_MAX_HOUR = 0.01
+    SCREENSHOT_DELAY_SEC = int(input(" * SCREENSHOT DELAY (SEC) [5, 10, 15] > "))
+    #SCREENSHOT_DELAY_SEC = 1
+    notify("Timelapse #screenshots in progress", f"{TIME_MAX_HOUR} hour remaining")
+    start_time = time.time()
     try:
-        run_screen(SCREENSHOT_DELAY_SEC,TIME_MAX_HOUR)
+        print(" * run_screen()")
+        run_screen(SCREENSHOT_DELAY_SEC, TIME_MAX_HOUR)
     except:
         print(" * error * go screens")
-
-    try:
-        print(" * go create timelapse")
-        create_timelapse(CACHE_DIR, params=f"({TIME_MAX_HOUR},{SCREENSHOT_DELAY_SEC}),{VIDEO_FPS}")
-    except:
-        print(" * error * go create timelapse")
     finally:
-        time.sleep(1)
-        remove(CACHE_DIR)
-    print("\n bye bye :) ")
-    time.sleep(5)
+        end_time_hour = convert_sec_to_hour((time.time() - start_time))
+        notify("Timelapse #screenshots finish", f"{round(end_time_hour,1)}h time ...")
+        create_timelapse(CACHE_DIR, params=f"({TIME_MAX_HOUR},{SCREENSHOT_DELAY_SEC}),{VIDEO_FPS}")
+        print("\n bye bye :) ")
+        time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
-
-
-
--- param.py
+++ param.py
@@ -1,4 +1,6 @@
 # photos
+import os
+
 SECRET_SCREENSHOTS_DIR_PATH = "h:/Desktop" # Where ? (just the dir)
 # result timelapse
 VIDEO_FPS = 15
